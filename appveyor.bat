8c7b4cef77c29ea42d1481bd959968e1de1be6a8
Maurits Lamers <maurits@weidestraat.nl>
2019-10-28 20:32:41 +0100

    first attempt at appveyor build

---
+++ appveyor.bat
@@ -0,0 +1,12 @@
+:: ----------------------------------------------------------------------
+:: Using msys2
+@echo on
+PATH C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
+:: set CHERE_INVOKING=yes
+@rem Reduce time required to install packages by disabling pacman's disk space checking.
+bash -lc "sed -i 's/^CheckSpace/#CheckSpace/g' /etc/pacman.conf"
+
+cd c:\gnucash-on-windows
+bash -lc "TARGET=gnucash-maint jhbuild -f jhbuildrc build"
+
+@echo off
