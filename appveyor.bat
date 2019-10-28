621ec0c0631f40dd8716155dd3d5254926daeb74
Maurits Lamers <maurits@weidestraat.nl>
2019-10-28 20:48:01 +0100

    working syntax, trying to call jhbuild through bash after first change dir

--- appveyor.bat
+++ appveyor.bat
@@ -6,7 +6,6 @@ PATH C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
 @rem Reduce time required to install packages by disabling pacman's disk space checking.
 bash -lc "sed -i 's/^CheckSpace/#CheckSpace/g' /etc/pacman.conf"
 
-cd c:\gnucash-on-windows
-TARGET=gnucash-maint jhbuild -f jhbuildrc build
+bash -lc "cd /c/gnucash-on-windows; TARGET=gnucash-maint jhbuild -f jhbuildrc build"
 
 @echo off
