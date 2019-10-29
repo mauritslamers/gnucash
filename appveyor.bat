9aeafd71a60e1f4da5259e0d6077c5a77018d5d9
Maurits Lamers <maurits@weidestraat.nl>
2019-10-29 12:08:24 +0100

    don't build docs

--- appveyor.bat
+++ appveyor.bat
@@ -4,6 +4,6 @@
 PATH C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
 :: set CHERE_INVOKING=yes
 
-bash -lc "cd /c/gnucash-on-windows; TARGET=gnucash-maint jhbuild -f jhbuildrc build"
+bash -lc "cd /c/gnucash-on-windows; TARGET=gnucash-maint jhbuild -f jhbuildrc build gnucash-git"
 
 @echo off
