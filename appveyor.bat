99167bad7bc9545cc40c77d3306840269416113b
Maurits Lamers <mauritslamers@gmail.com>
2019-10-30 15:30:10 +0100

    attempt at using msys2_shell.cmd to start jhbuild

--- appveyor.bat
+++ appveyor.bat
@@ -4,6 +4,7 @@
 PATH C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%
 :: set CHERE_INVOKING=yes
 
-bash -lc "cd /c/gnucash-on-windows; TARGET=gnucash-maint jhbuild -f jhbuildrc build gnucash-git"
+:: bash -lc "cd /c/gnucash-on-windows; TARGET=gnucash-maint jhbuild -f jhbuildrc build gnucash-git"
+msys2_shell.cmd "cd /c/gnucash-on-windows; TARGET=gnucash-maint jhbuild -f jhbuildrc build gnucash-git"
 
 @echo off
